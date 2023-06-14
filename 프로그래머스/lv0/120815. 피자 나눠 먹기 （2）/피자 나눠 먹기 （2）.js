2735e0fb05f88b73b95c97788cf6322b52d03068
Young-Min <82636319+ddengmin@users.noreply.github.com>
2023-06-14 23:44:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let pizza = 6;
+    while (pizza % n !== 0) {
+        pizza = pizza + 6
+        //입출력 예 #1 (남겨줄 경우)
+    }
+    return pizza / 6;
+    //입출력 예 #2 (남겨지지 않을 경우)
+}
